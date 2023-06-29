d3775cc94acd20069c1e832bff06d77c669751c3
vicdd <victorrobcam@gmail.com>
2023-06-29 13:10:18 -0300

    added round simulation

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ function getComputerChoice() {
             return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            return "Tie!";
+        } else if (computerSelection == 'paper') {
+            return "Computer Wins!";
+        } else {
+            return "Player Wins!";
+        }
+    }
+    
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            return "Player Wins!";
+        } else if (computerSelection == 'paper') {
+            return "Tie!";
+        } else {
+            return "Computer Wins!";
+        }
+    }
+
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            return "Computer Wins!";
+        } else if (computerSelection == 'paper') {
+            return "Player Wins!";
+        } else {
+            return "Tie!";
+        }
+    }
+    return "None";
+}
+
